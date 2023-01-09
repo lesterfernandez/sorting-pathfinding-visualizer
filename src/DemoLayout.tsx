6a455615d3b41dcc30bb9e2cbd31b7414856e7e0
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2023-01-09 18:01:15 -0500

    feat: make topbar responsive for mobile

--- src/DemoLayout.tsx
+++ src/DemoLayout.tsx
@@ -11,9 +11,9 @@ export function DemoLayout({ children }: Props) {
       <h1 className="pt-10 pb-14 text-center text-4xl font-medium text-blue-500">
         Algorithm Visualizer
       </h1>
-      <section className="mx-2 mb-2 flex flex-grow flex-col sm:mx-8 sm:mb-8 lg:mx-auto lg:min-w-[1024px]">
+      <section className="flex w-full max-w-screen-lg flex-grow flex-col px-2 pb-2 sm:px-8 sm:pb-8 lg:mx-auto">
         <DemoTopBar />
-        <div className="flex-1 rounded-xl border-4 border-amber-100 shadow">
+        <div className="rounded-x-xl flex-1 rounded-b-xl border-4 border-amber-100 shadow sm:rounded-t-xl">
           {children}
         </div>
       </section>
--- src/DemoTopBar.tsx
+++ src/DemoTopBar.tsx
@@ -2,31 +2,29 @@ import { NavLink } from "react-router-dom";
 
 export function DemoTopBar() {
   return (
-    <div className="flex h-10 max-w-4xl justify-around md:gap-28 md:self-center">
-      <button className="rounded-t-lg bg-gray-100 px-4 hover:text-blue-500">
+    <div className="grid h-24 max-w-4xl grid-cols-2 grid-rows-2 justify-center sm:flex sm:h-10 sm:justify-around md:self-center">
+      <button className="col-start-2 border-t-2 border-r-2 bg-gray-100 px-4 hover:text-blue-500 sm:ml-4 sm:rounded-t-lg sm:border-l-2 md:mr-24">
         Configure
       </button>
-      <div className="peer-[]: flex rounded-t-xl text-center last:border-l-0">
-        <NavLink
-          to="/sorting"
-          className={({ isActive }) =>
-            "flex items-center rounded-tl-lg border-x-2 border-t-2 p-4" +
-            (isActive ? " bg-gray-100 text-blue-500" : "")
-          }
-        >
-          Sorting
-        </NavLink>
-        <NavLink
-          to="/pathfinding"
-          className={({ isActive }) =>
-            "flex items-center rounded-tr-lg border-r-2 border-t-2 p-4" +
-            (isActive ? " bg-gray-100 text-blue-500" : "")
-          }
-        >
-          Pathfinding
-        </NavLink>
-      </div>
-      <button className="rounded-t-lg bg-gray-100 px-4 hover:text-blue-500">
+      <NavLink
+        to="/sorting"
+        className={({ isActive }) =>
+          "row-start-1 flex place-content-center items-center border-2 border-b-0 p-4 sm:ml-auto sm:rounded-tl-lg" +
+          (isActive ? " bg-gray-100 text-blue-500" : "")
+        }
+      >
+        Sorting
+      </NavLink>
+      <NavLink
+        to="/pathfinding"
+        className={({ isActive }) =>
+          "flex place-content-center items-center border-2 p-4 sm:mr-auto sm:rounded-tr-lg sm:border-b-0 sm:border-l-0" +
+          (isActive ? " bg-gray-100 text-blue-500" : "")
+        }
+      >
+        Pathfinding
+      </NavLink>
+      <button className="border-2 border-l-0 bg-gray-100 px-4 hover:text-blue-500 sm:mr-4 sm:rounded-t-lg sm:border-b-0 sm:border-l-2 md:ml-24">
         Visualize
       </button>
     </div>
